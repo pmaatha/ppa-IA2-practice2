006681e79f8e7267e60a4401f440e5e16463a496
Mamatha P <97168002+pmaatha@users.noreply.github.com>
2022-02-09 13:37:36 +0000

    p1original.c complited

--- p1original.c
+++ p1original.c
@@ -1,2 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int base,height;
 
+  float area;
+  
+  printf("enter the value of base=\n");
+  scanf("%d",&base);
+  printf("enter the value of height= ");
+  scanf("%d",&height);
+  area=0.5*base*height;
+  printf("area of triangle=%f",area);
+  return ;
 }
+
+
+
+
+
